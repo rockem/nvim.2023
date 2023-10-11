eb46c7dd5871ffd6be9331c93e432fed8bdb0334
Eli Segal <eli.segal@gmail.com>
2023-10-11 23:29:16 +0300

    More updates

--- lua/rockem/plugins/colorscheme.lua
+++ lua/rockem/plugins/colorscheme.lua
@@ -1,8 +1,20 @@
 return {
-	"bluz71/vim-nightfly-guicolors",
-	priority = 1000, -- Make sure this loads before all others
-	config = function()
-		-- load colorscheme here
-		vim.cmd([[colorscheme nightfly]])
-	end,
-}
+  {
+    "bluz71/vim-nightfly-guicolors",
+    priority = 1000, -- Make sure this loads before all others
+    config = function()
+      -- load colorscheme here
+      vim.cmd([[colorscheme nightfly]])
+    end,
+  }, {
+    "savq/melange-nvim",
+    version = false,
+    lazy = false,
+    priority = 1000,
+  }, {
+    "ishan9299/modus-theme-vim",
+    version = false,
+    lazy = false,
+    priority = 1000,
+  }
+} 
